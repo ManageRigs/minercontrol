c987b0eeb368db8420fbeb65e3b17ae4752c277c
rigsmanage <rigsmanage@gmail.com>
2019-02-12 19:41:26 +0200

    v 1.1.3

--- src/windows/managerigs.py
+++ src/windows/managerigs.py
@@ -1,6 +1,6 @@
 import main_monitor.main_monitor as main_monitor
 
-# Current program version
-version = (1, 1, 2)
+# Current Linux program version
+version = (1, 1, 3)
 
 main_monitor.main(version)
